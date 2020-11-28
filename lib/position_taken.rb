14cdb54737bccf9341603950f2dde3bdfe74db32
Danny Brandt <dvbrandt90@gmail.com>
2020-11-28 07:24:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index] == " " || board[index] == nil
+  check = board[index]
+  if check == " " || check == nil || check == ""
     false 
   else true 
   end
